c7d5d7b011643fdf8a85db0b6af5b12b032a0ecb
tandm <tandm@tandm.local>
2023-02-08 14:48:32 +0700

    [init] - Vite Standard Template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Welcome } from '$components';
+
+const App = () => (
+	<div>
+		<Welcome />
+	</div>
+);
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+const Welcome = () => (
+	<div>
+		<h3>Welcome</h3>
+		<div>Welcome - Content</div>
+	</div>
+);
+
+export default Welcome;
